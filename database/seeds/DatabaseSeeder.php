c2059ad0ef4abea41a22f4effca4964515cf83eb
Glenn Espejo <glenn.espejo0112.com>
2019-11-12 18:47:12 +0800

    add migration, seeder and model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\UserComment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user_1 = new User();
+        $user_1->id = '1';
+        $user_1->name = 'John Smith';
+        $user_1->save();
+
+        $user_2 = new User();
+        $user_2->id = '2';
+        $user_2->name = 'John Doe';
+        $user_2->save();
+
+        $user_comment_1 = new UserComment();
+        $user_comment_1->user_id = 1;
+        $user_comment_1->comment = 'Test Comment 1';
+
+        $user_comment_2 = new UserComment();
+        $user_comment_2->user_id = 2;
+        $user_comment_2->comment = 'Test Comment 2';
     }
 }
